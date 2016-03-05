16df2c6ee7afb6027154a9609abb05088cd2c6d5
Spencer <spencer.whitehead@rogers.com>
2016-03-05 13:58:24 -0500

    Added ace, editor can now be included for problems

---
+++ js/editor.js
@@ -0,0 +1,15 @@
+var editor = ace.edit(editor);
+editor.setTheme("/js/ace/theme/monokai");
+editor.getSession().setMode("/js/ace/mode/c");
+$(function() {
+	$('.dropdown-toggle').dropdown();
+	$('#set-lang-c').click(function() {
+		editor.getSession().setMode("/js/ace/mode/c");
+	});
+	$('#set-lang-c++').click(function() {
+
+	});
+	$('#set-lang-ruby').click(function() {
+		editor.getSession().setMode("/js/ace/mode/ruby");
+	});
+});
