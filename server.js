3523d2749dbc6b57fe47f2f57f8fd29b8e4624a6
Spencer <spencer.whitehead@rogers.com>
2016-03-04 22:46:48 -0500

    A less terrible way of partials
    Implemented partials using EJS. As a result of this, we now store pages
    in the views/pages folder, partials in the views/partials folder. Pages
    must be specified to be rendered in server.js (find a good way to forall
    them later?) Also, name files .EJS not .HTML now

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ var express = require('express');
 
 var server = express();
 
+server.set('view engine', 'ejs');
+
 server.use('/', express.static(__dirname + '/'));
+
+server.get('/', function(req, res) {
+	res.render('pages/index');
+});
 
 server.listen(8080, function() {
 	console.log("Listening on 8080");
