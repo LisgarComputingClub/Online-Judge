e7d6f3d0c8ab9d92b465222a34d935b8c9af9317
Spencer <spencer.whitehead@rogers.com>
2016-03-04 23:20:26 -0500

    Stuff works now

--- server.js
+++ server.js
@@ -8,7 +8,35 @@ server.set('view engine', 'ejs');
 server.use('/', express.static(__dirname + '/'));
 
 server.get('/', function(req, res) {
-	res.render('pages/index');
+	res.render('pages/index',{title:"LCI Online Judge"});
+});
+
+server.get('/index', function(req, res) {
+	res.render('pages/index',{title:"LCI Online Judge"});
+});
+
+server.get('/profile', function(req, res) {
+	res.render('pages/profile',{title:"Profile"});
+});
+
+server.get('/users', function(req, res) {
+	res.render('pages/users',{title:"Users"});
+});
+
+server.get('/contests', function(req, res) {
+	res.render('pages/contests',{title:"Contests"});
+});
+
+server.get('/problems', function(req, res) {
+	res.render('pages/problems',{title:"Problems"});
+});
+
+server.get('/about', function(req, res) {
+	res.render('pages/about',{title:"About"});
+});
+
+server.get('/organizations', function(req, res) {
+	res.render('pages/organizations',{title:"Organizations"});
 });
 
 server.listen(8080, function() {
