6d9901906d1cea424111cb6a590fba5f3d977ec5
Spencer <spencer.whitehead@rogers.com>
2016-03-06 12:18:08 -0500

    a

--- website.js
+++ website.js
@@ -1,52 +1,44 @@
 var http = require('http');
 var express = require('express');
 
-var server = express();
+exports.server = express();
 
-server.set('view engine', 'ejs');
+exports.server.set('view engine', 'ejs');
 
-server.use('/', express.static(__dirname + '/'));
+exports.server.use('/', express.static(__dirname + '/'));
 
-server.get('/', function(req, res) {
+exports.server.get('/', function(req, res) {
 	res.render('pages/index',{title:"LCI Online Judge"});
 });
 
-server.get('/index', function(req, res) {
+exports.server.get('/index', function(req, res) {
 	res.render('pages/index',{title:"LCI Online Judge"});
 });
 
-server.get('/profile', function(req, res) {
+exports.server.get('/profile', function(req, res) {
 	res.render('pages/profile',{title:"Profile"});
 });
 
-server.get('/users', function(req, res) {
+exports.server.get('/users', function(req, res) {
 	res.render('pages/users',{title:"Users"});
 });
 
-server.get('/contests', function(req, res) {
+exports.server.get('/contests', function(req, res) {
 	res.render('pages/contests',{title:"Contests"});
 });
 
-server.get('/problems', function(req, res) {
+exports.server.get('/problems', function(req, res) {
 	res.render('pages/problems',{title:"Problems"});
 });
 
-server.get('/about', function(req, res) {
+exports.server.get('/about', function(req, res) {
 	res.render('pages/about',{title:"About"});
 });
 
-server.get('/organizations', function(req, res) {
+exports.server.get('/organizations', function(req, res) {
 	res.render('pages/organizations',{title:"Organizations"});
 });
 
-server.get(/\/problems\//, function(req, res) {
-	res.render('pages/problems/' + req.url.substr(10), {title:req.url.substr(10)});
-});
-
-server.get('/submit', function(req, res) {
-	console.log("request sent to /submit");
-});
-
-server.listen(8080, function() {
+exports.server.listen(8080, function() {
 	console.log("Listening on 8080");
 });
