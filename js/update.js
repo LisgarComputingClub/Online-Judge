42652fec14397b3292821497ceae54b06c8f797c
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-03-12 22:46:12 -0500

    Added auto-update functionality for Windows. Will test soon.

--- js/update.js
+++ js/update.js
@@ -1,5 +1,6 @@
 // This module automatically checks for updates from the Github repo
-// We're using child process
+// We're using OS and child process
+var os = require("os");
 var exec = require("child_process").exec;
 
 // This function executes a bash command asynchronously
@@ -11,14 +12,45 @@ function execute(command, callback) {
 
 // Run a command to check for updates
 module.exports.checkUpdate = function(callback) {
+    // Check if runing Linux
+    if (os.platform() == "linux") {
+        execute("git fetch && git status", function(output) {
+            if (output.indexOf("Your branch is behind") > -1) {
+                console.log("Automatically installing latest update.");
+                
+                execute("bash update.sh");
+                process.exit();
+            } else {
+                console.log("No updates available.");
+            }
+        });
+    } else {
+        // Not running Linux
+        console.log("Sorry, auto-update only works on Linux.");
+    }
+    // Check platform
+};
+
+module.exports.checkUpdate = function(callback) {
     execute("git fetch && git status", function(output) {
         if (output.indexOf("Your branch is behind") > -1) {
             console.log("Automatically installing latest update.");
             
-            execute("bash update.sh");
-            process.exit();
+            // Check platform
+            switch(os.platform()) {
+                case "linux":
+                    execute("bash update.sh");
+                    break;
+                case "win32":
+                    execute("update.bat");
+                    break;
+                default:
+                    console.log("Sorry, your platform does not support auto-updating.");
+                    break;
+            }
+            
         } else {
             console.log("No updates available.");
         }
     });
-};
+}
---
+++ update.bat
@@ -0,0 +1,3 @@
+::Pull the latest version of the Git repo
+echo Pulling latest version of the online judge...
+git pull
