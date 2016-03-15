dba45d99e45a8094bedec21a526b4ae52a4fadbe
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-03-15 01:06:28 -0400

    Added update checking every hour.

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-// Check for updates
+// Include update checker
 var update = require("./js/update.js");
-update.checkUpdate();
 
 // Start the website
 var website = require("./website.js");
+
+// Check for update on startup
+update.checkUpdate();
+
+// Check for updates every hour
+setInterval(update.checkUpdate(), 3600000);
