3a4edaefb0e843e748ff74a4739661b4a46368fd
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-03-15 03:48:28 -0400

    updateLanguages.js is now completely synchronous... that's what we're supposed to do with Node right?

--- js/updateLanguages.js
+++ js/updateLanguages.js
@@ -1,20 +1,24 @@
-// HTTP for download and FS for saving to file
-var http = require("http");
+// Sync-request for download
+var request = require("sync-request");
+
+// FS for writing file
 var fs = require("fs");
 
+// Synchronous file checking
+var exists = require("./exists.js").exists;
+
 module.exports.updateLanguages = function() {
+
     console.log("Updating languages.");
 
     // Delete languages.json if exists
-    fs.exists("languages.json", function(exists) {
-        if (exists) {
-            fs.unlink("languages.json");
-        }
-    });
+    if (exists("languages.json")) {
+        fs.unlink("languages.json");
+    }
 
     // Download new languages.json
-    var file = fs.createWriteStream("lanuages.json");
-    var request = http.get("http://api.hackerrank.com/checker/languages.json", function(response) {
-        response.pipe(file);
-    });
+    var res = request("GET", "http://api.hackerrank.com/checker/languages.json");
+    
+    // Write to file
+    fs.writeFileSync("languages.json", res.getBody());
 };
