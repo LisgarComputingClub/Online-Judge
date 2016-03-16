44b32b3dd35bf847cd62e98f2bc4ece877b510f2
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-03-16 18:45:44 -0400

    Updated the exists module to ahve both sync and async functions.

--- js/exists.js
+++ js/exists.js
@@ -1,13 +1,23 @@
 // Use filesystem
 var fs = require("fs");
 
-module.exports.exists = function(file) {
-    try
-    {
+module.exports.exists = function(file, callback) {
+    fs.stat(file, function(err, stat) {
+        if(err === null) {
+            callback(true);
+        } else if(err.code == 'ENOENT') {
+            callback(false);
+        } else {
+            callback(err);
+        }
+    });
+};
+
+module.exports.existsSync = function(file) {
+    try {
         return fs.statSync(file).isFile();
     }
-    catch (err)
-    {
+    catch (err) {
         return false;
     }
 };
