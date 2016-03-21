5c174e1e6b0a061f479517ea166c13248fb4c1c4
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-03-21 17:24:01 -0400

    Added a submit function that evaluates a file of code (evaluateFile).

--- js/submit.js
+++ js/submit.js
@@ -2,8 +2,10 @@
 var fs = require("fs");
 var HackerRank = require("machinepack-hackerrank");
 
+// Get languages file
 var languages = JSON.parse(fs.readFileSync('languages.json', 'utf8')).languages.codes;
 
+// Submit a string of code
 module.exports.evaluateCode = function(code, language, testcases, answers, callback) {
     var results = [];
     HackerRank.submit({
@@ -18,6 +20,36 @@ module.exports.evaluateCode = function(code, language, testcases, answers, callb
         error: function(err) {
             throw err;
         },
+        // Code compiled successfully
+        success: function(response) {
+            JSON.parse(response).result.stdout.forEach(function(val, index, array) {
+                if(val == answers[index]) {
+                    results.push(true);
+                } else {
+                    results.push(false);
+                }
+            });
+            callback(results);
+        }
+    });
+};
+
+// Submit a file with code
+module.exports.evaluateFile = function(file, language, testcases, answers, callback) {
+    var results = [];
+    HackerRank.submitFile({
+        apiKey: 'hackerrank|731195-684|a196c8ef286bf980b8b79ba0cff378e550678d5e',
+        filePath: file,
+        language: languages[language],
+        testcases: testcases,
+        wait: true,
+        format: "json"
+    }).exec({
+        // Unexpected error
+        error: function(err) {
+            console.log(err);
+        },
+        // Code compiled successfully
         success: function(response) {
             JSON.parse(response).result.stdout.forEach(function(val, index, array) {
                 if(val == answers[index]) {
