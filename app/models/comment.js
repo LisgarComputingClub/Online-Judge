ef1f90263bab366fef4cada5d3615b440dc7e8f2
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-08-10 09:07:53 -0400

    Added a comment schema.

---
+++ app/models/comment.js
@@ -0,0 +1,15 @@
+// Packages
+var mongoose = require("mongoose");
+
+// Comment schema
+var commentchema = mongoose.Schema({
+    contentType: String,
+    author: String,
+    creationDate: Date,
+    lastEdit: Date,
+    content: String,
+    deleted: Boolean
+});
+
+// Create and export the model
+module.exports = mongoose.model("Comment", commentSchema);
