35154c4238bfce40dd95483b10e29c00094a3236
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-08-10 10:18:39 -0400

    Added failasfe to ensure that comments get loaded. This fixes a weird glitch.

--- public/js/comments.js
+++ public/js/comments.js
@@ -8,6 +8,8 @@ socket.on("connect", function () {
 
 // Recieve comments
 socket.on("comments-response", function (data) {
+    // Comments are loaded
+    commentsLoaded = true;
     // Clear comments
     $("#comments-list").empty();
     // Add each comment to the list
@@ -15,4 +17,26 @@ socket.on("comments-response", function (data) {
         // Add a list item
         $("#comments-list").append('<a class="list-group-item"><h4 class="list-group-item-heading"><span class="comment-author">' + val.author + '</span> said</h4><p class="list-group-item-text">' + val.content + '</p></a>');
     });
+});
+
+// Ensure comments are loaded
+var commentsLoaded = false;
+
+function checkCommentsLoaded() {
+    if (!commentsLoaded) {
+        // Request comments
+        socket.emit("comments-request", {
+            type: "problem",
+            pid: pid
+        });
+        // Set a timeout to check if comments are loaded again
+        setTimeout(function () {
+            checkCommentsLoaded();
+        });
+    }
+}
+
+// Set a timeout to check if comments are loaded
+setTimeout(function () {
+    checkCommentsLoaded();
 });
